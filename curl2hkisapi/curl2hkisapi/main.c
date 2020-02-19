1ddaa16aa882a818e13c85d8acf4345bb962b380
Zhang <892737105@qq.com>
2020-02-19 10:27:35 +0800

    curl init
    Signed-off-by: Zhang <892737105@qq.com>

--- curl2hkisapi/curl2hkisapi/main.c
+++ curl2hkisapi/curl2hkisapi/main.c
@@ -3,11 +3,20 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define SZY_LOG(fmt, ...)			printf(fmt, ##__VA_ARGS__)
+#include <curl/curl.h>
+
+#define SZY_LOG(fmt, ...)			printf(fmt, ##__VA_ARGS__); putchar('\n')
 
 int main(void)
 {
-	SZY_LOG("start !");
+	//  must perform in main thread before any curl operating
+	//  NOTE
+	//    This function is not thread safe.
+	curl_global_init(CURL_GLOBAL_ALL);
+
+	curl_version_info_data* info = curl_version_info(CURLVERSION_NOW);// no need for NULL check
+	SZY_LOG("curl version : %s. async name lookup is %s.", info->version, \
+		(info->features & CURL_VERSION_ASYNCHDNS) ? "YES" : "NO");
 
 	return 0;
 }
