ed38e54bfbf753730ec3c65ea5eb8df83702b3f6
Zhang <892737105@qq.com>
2020-02-19 11:32:18 +0800

    body

--- curl2hkisapi/curl2hkisapi/main.c
+++ curl2hkisapi/curl2hkisapi/main.c
@@ -5,7 +5,43 @@
 
 #include <curl/curl.h>
 
+/************************************************************************/
+/*                                                                      */
+/************************************************************************/
+
 #define SZY_LOG(fmt, ...)			printf(fmt, ##__VA_ARGS__); putchar('\n')
+
+/************************************************************************/
+/*                                                                      */
+/************************************************************************/
+
+static uint8_t g_http_get_body[4096];
+static size_t g_http_get_body_size;
+static size_t g_http_get_body_len;
+
+static size_t http_get_write_body(void* ptr, size_t size, size_t nmemb, void* stream)
+{
+	uint8_t* body = stream;
+
+	if (g_http_get_body_size - g_http_get_body_len >= size * nmemb)
+	{
+		memcpy(&g_http_get_body[g_http_get_body_len], ptr, size * nmemb);
+		g_http_get_body_len += size * nmemb;
+
+		return size * nmemb;
+	}
+	else
+	{
+		memcpy(&g_http_get_body[g_http_get_body_len], ptr, g_http_get_body_size - g_http_get_body_len);
+		g_http_get_body_len = g_http_get_body_size;
+
+		return g_http_get_body_size - g_http_get_body_len;
+	}
+}
+
+/************************************************************************/
+/*                                                                      */
+/************************************************************************/
 
 int main(void)
 {
@@ -30,6 +66,12 @@ int main(void)
 
 		res_code = curl_easy_setopt(http_get, CURLOPT_URL, "http://172.16.51.9/ISAPI/Security/userCheck");
 
+		memset(g_http_get_body, 0, sizeof(g_http_get_body));
+		g_http_get_body_size = sizeof(g_http_get_body);
+		g_http_get_body_len = 0;
+		res_code = curl_easy_setopt(http_get, CURLOPT_WRITEFUNCTION, http_get_write_body);
+		res_code = curl_easy_setopt(http_get, CURLOPT_WRITEDATA, g_http_get_body);
+
 		//////////////////////////////////////////////////////////////////////////
 
 		res_code = curl_easy_perform(http_get);
@@ -41,6 +83,10 @@ int main(void)
 		else
 		{
 			SZY_LOG("请求认证证书 Succ");
+
+			SZY_LOG("========================================");
+			SZY_LOG("body => %s", g_http_get_body);
+			SZY_LOG("========================================");
 		}
 
 		//////////////////////////////////////////////////////////////////////////
